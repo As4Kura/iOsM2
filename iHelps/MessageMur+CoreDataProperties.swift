5aa74529ab2b0edda7b82a9b414c12f04020eeb8
nicolas catanoso <nicos445@MacBook-Pro-de-Moi.local>
2016-03-21 19:37:17 +0100

    maj BDD/jeuDeDonne/MM/Annonce

--- iHelps/MessageMur+CoreDataProperties.swift
+++ iHelps/MessageMur+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  MessageMur+CoreDataProperties.swift
 //  iHelps
 //
-//  Created by Moi on 14/03/2016.
+//  Created by nicolas catanoso on 21/03/2016.
 //  Copyright © 2016 NXN. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -15,6 +15,7 @@ import CoreData
 extension MessageMur {
 
     @NSManaged var contenu: String?
+    @NSManaged var dateMM: NSDate?
     @NSManaged var emetteur: Utilisateur?
     @NSManaged var serviceG: ServiceGlobal?
 
