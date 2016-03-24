75a9c7a9b1059e307517f4897ee1eb5cda456cf2
Nicolas <asakura@ymail.com>
2016-03-24 19:33:15 +0100

    Ajout des appels de base pour la notation

--- iHelps/Note_ViewController.swift
+++ iHelps/Note_ViewController.swift
@@ -10,26 +10,25 @@ import UIKit
 
 class Note_ViewController: UIViewController {
 
-    
-    
     @IBOutlet weak var nomService: UILabel!
     @IBOutlet weak var prenomPropo: UILabel!
     @IBOutlet weak var noteEtoiles: CosmosView!
     @IBOutlet weak var textCom: UITextView!
     @IBOutlet weak var validerReview: UIButton!
     
-    /*
-    @IBOutlet weak var labelNomService: UILabel!
-    @IBOutlet weak var labelPrenomUtil: UILabel!
-    @IBOutlet weak var ratingStars: CosmosView!
-    @IBOutlet weak var textCommentaire: UITextView!
-    @IBOutlet weak var buttonValider: UIButton!
-    */
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Changer la note
+        noteEtoiles.rating = 4
+        
+        // Called when user finishes changing the rating by lifting the finger from the view.
+        // This may be a good place to save the rating in the database or send to the server.
+        noteEtoiles.didFinishTouchingCosmos = { rating in }
+        
+        // A closure that is called when user changes the rating by touching the view.
+        // This can be used to update UI as the rating is being changed by moving a finger.
+        noteEtoiles.didTouchCosmos = { rating in }
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,15 +36,5 @@ class Note_ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
